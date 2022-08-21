c420ad4bd34ed057ba521ac31d148e194813a0ff
ashley <ashley.oketo@student.moringaschool.com>
2022-08-21 13:52:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+  let doc  = document.querySelector('p');
+  doc.textContent = "This is really cool!"
+  console.log(document.querySelector('p').textContent) 
+});
+
+console.log(document.querySelector('p').textContent);
